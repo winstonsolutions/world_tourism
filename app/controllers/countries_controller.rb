39e78de07e1f75e839af51596c98ee160d5a3365
winston <andyredjohn@gmail.com>
2025-03-18 12:11:32 -0500

    show 4 citys

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -5,6 +5,6 @@ class CountriesController < ApplicationController
 
   def show
     @country = Country.find(params[:id])
-    @cities = @country.cities.order(:name).page(params[:page]).per(5)
+    @cities = @country.cities.order(:name).page(params[:page]).per(4)
   end
 end
